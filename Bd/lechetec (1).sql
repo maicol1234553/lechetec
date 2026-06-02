f505aad4d10b28450780fbaa8f1f8a97dae265e8
maicol1234553 <maicolroncancio2005@gmail.com>
2026-06-01 20:55:56 -0500

    base de datos actualizada

--- Bd/lechetec (1).sql
+++ Bd/lechetec (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2026 a las 22:56:29
+-- Tiempo de generación: 02-06-2026 a las 03:55:24
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -52,7 +52,8 @@ INSERT INTO `analisis_fq` (`ID_AFQ`, `ID_MUE`, `ACI_AFQ`, `DEN_AFQ`, `GRA_AFQ`, 
 (6, 6, 0.01, -0.01, 0.01, 0.02, 0.01, 0.01, 0),
 (7, 7, 0.01, -0.01, 0.01, 0.02, 0.01, 0.01, 0),
 (8, 8, 1.95, 1.002, 0.06, 0.04, 0.03, 0.05, 0),
-(9, 9, 0.01, 1.001, 0.02, 0.01, 0.02, 0.02, 0);
+(9, 9, 0.01, 1.001, 0.02, 0.01, 0.02, 0.02, 0),
+(10, 10, 15, 1.008, 0.02, 0.02, 0.02, 0.03, 0);
 
 -- --------------------------------------------------------
 
@@ -83,7 +84,8 @@ INSERT INTO `analisis_micro` (`ID_MIC`, `ID_MUE`, `UFC_MIC`, `COL_MIC`, `MES_MIC
 (6, 6, 0, 0, 0, 1, 'hhhh'),
 (7, 7, 0, 0, 0, 0, 'kkk'),
 (8, 8, 0, 1, 1, 1, 'buena '),
-(9, 9, 0, 1, 1, 1, 'estado buena ');
+(9, 9, 0, 1, 1, 1, 'estado buena '),
+(10, 10, 0, 1, 1, 1, 'leche en buen estado');
 
 -- --------------------------------------------------------
 
@@ -115,7 +117,8 @@ INSERT INTO `muestra` (`ID_MUE`, `FEC_MUE`, `HOR_MUE`, `ORI_MUE`, `RES_MUE`, `us
 (6, '2026-05-05', '02:11:15', '5', '56', NULL, NULL, 2),
 (7, '2026-05-14', '19:12:37', '5', '56', NULL, NULL, 1),
 (8, '2026-05-22', '13:04:10', 'Instituto Tecnológico de Roque', 'Maicol fernando Roncancio ', NULL, NULL, 1),
-(9, '2026-05-22', '14:51:07', 'Instituto Tecnológico de Roque', 'camila', NULL, NULL, 8);
+(9, '2026-05-22', '14:51:07', 'Instituto Tecnológico de Roque', 'camila', NULL, NULL, 8),
+(10, '2026-05-28', '09:24:42', 'Instituto Tecnológico de Roque', 'Maicol fernando Roncancio ', NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -129,20 +132,24 @@ CREATE TABLE `usuarios` (
   `password` varchar(255) NOT NULL,
   `rol` enum('admin','usuario') NOT NULL,
   `numero_control` varchar(50) DEFAULT NULL,
-  `estado` enum('activo','inactivo') DEFAULT 'activo'
+  `estado` enum('activo','inactivo') DEFAULT 'activo',
+  `correo` varchar(100) NOT NULL,
+  `frase_secreta` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `password`, `rol`, `numero_control`, `estado`) VALUES
-(1, 'maicol', '$2y$10$2kYPUIbce06OOJAmVXJfD.3KDS/daHmrLSF6Ya/kSHUiVzs2PQCcq', 'admin', '12345', 'activo'),
-(2, 'maicol1', '$2y$10$S8qfw5JomlmEVGEDPdEX0.h2H.GbvNGC9cOu3Dx16xv79gSFunpdC', 'usuario', '123456', 'activo'),
-(3, 'esteban ', '$2y$10$MPp7rHukzb2He3gQyWm9uO4GWHuRRWyxTMYZvYBhMjA2xwIiXtKH.', 'usuario', '132', 'activo'),
-(4, 'fer', '$2y$10$TV/FfGQouWVsxGjvz3g/EuRGmvKNUVXr9bDUW85HuuYqmU0vzTBf6', 'usuario', '11', 'activo'),
-(7, 'anibal ', '$2y$10$bG37RXpkoZgxngpGmNRmOez9MCO6pX0FmVJzQoUkIiheb4URvlgQG', 'usuario', '823918', 'activo'),
-(8, 'camila', '$2y$10$BI1sXUHb91JE1OY.fBwlx.I1ORw9v7InoR815SVicx22avypFf0/m', 'usuario', '0000', 'activo');
+INSERT INTO `usuarios` (`id`, `usuario`, `password`, `rol`, `numero_control`, `estado`, `correo`, `frase_secreta`) VALUES
+(1, 'maicol', '$2y$10$YV38Bl/GlpzDGfrmJUS6DuiButpyCG3HN9vUsCPNYkNmSvo3/cU..', 'admin', '12345', 'activo', '', 'hola'),
+(2, 'maicol1', '$2y$10$S8qfw5JomlmEVGEDPdEX0.h2H.GbvNGC9cOu3Dx16xv79gSFunpdC', 'usuario', '123456', 'activo', '', 'hola'),
+(3, 'esteban ', '$2y$10$MPp7rHukzb2He3gQyWm9uO4GWHuRRWyxTMYZvYBhMjA2xwIiXtKH.', 'usuario', '132', 'activo', '', NULL),
+(4, 'fer', '$2y$10$TV/FfGQouWVsxGjvz3g/EuRGmvKNUVXr9bDUW85HuuYqmU0vzTBf6', 'usuario', '11', 'activo', '', NULL),
+(7, 'anibal ', '$2y$10$bG37RXpkoZgxngpGmNRmOez9MCO6pX0FmVJzQoUkIiheb4URvlgQG', 'usuario', '823918', 'activo', '', NULL),
+(8, 'camila', '$2y$10$BI1sXUHb91JE1OY.fBwlx.I1ORw9v7InoR815SVicx22avypFf0/m', 'usuario', '0000', 'activo', '', NULL),
+(9, 'padro', '$2y$10$YrfidncWtKtuFfg1uwZe8.g.tKF/5rliTrzFj.en/W4QJMvyYK1nC', 'usuario', '444', 'activo', '', NULL),
+(10, 'fernadno', '$2y$10$2A3dMoii9.rdkD49zuUR2.nmvD6OZKEP48Rk7xdwxJjQmCmZKEIu.', 'usuario', '11', 'activo', '', 'hola');
 
 --
 -- Índices para tablas volcadas
@@ -183,25 +190,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `analisis_fq`
 --
 ALTER TABLE `analisis_fq`
-  MODIFY `ID_AFQ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID_AFQ` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `analisis_micro`
 --
 ALTER TABLE `analisis_micro`
-  MODIFY `ID_MIC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID_MIC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `muestra`
 --
 ALTER TABLE `muestra`
-  MODIFY `ID_MUE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID_MUE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Restricciones para tablas volcadas
